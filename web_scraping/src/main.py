70daf6115496f471b6084f2aef38e06a7830cf9d
zeyneptekin <zeyneptekin1682@gmail.com>
2024-07-31 01:49:56 +0300

    adjust folder path

--- web_scraping/src/main.py
+++ web_scraping/src/main.py
@@ -1,11 +1,11 @@
-from web_scraping.src.helpers.get_products_name import get_products_name
-from web_scraping.src.helpers.get_products_prices import get_products_prices
+from helpers.get_products_name import get_products_name
+from helpers.get_products_prices import get_products_prices
 
 
 def main():
     product_names = get_products_name()
 
-    country_file = '/app/countries.txt'
+    country_file = '/app/src/countries.txt'
     get_products_prices(country_file, product_names)
 
 
