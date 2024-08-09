4247738b65f3888375f8214ba5e1a066b2c307c4
zeyneptekin <zeyneptekin1682@gmail.com>
2024-08-10 02:27:11 +0300

    [adjust] text size & bg color in footer & navbar

--- web/src/components/footer/index.tsx
+++ web/src/components/footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="fixed bottom-0 bg-amber-100 w-full h-10 items-center justify-center flex">
-            <p>by zeyneptekin</p>
+            <p className="italic">by zeyneptekin</p>
         </footer>
     )
 }
--- web/src/components/navbar/index.tsx
+++ web/src/components/navbar/index.tsx
@@ -1,12 +1,9 @@
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 bg-gray-400 w-full h-16 flex items-center px-8 justify-between">
+        <nav className="fixed top-0 bg-amber-100 w-full h-16 flex items-center px-8 justify-between text-xl">
             <p>Global Price Tracking</p>
             <ul className="flex gap-3">
-                <li>aaa</li>
-                <li>aaa</li>
-                <li>aaa</li>
-                <li>aaa</li>
+                <li>About</li>
             </ul>
         </nav>
     )
