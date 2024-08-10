f04c6cb6e517d60a273a8e57b19698dedd92bd7d
zeyneptekin <zeyneptekin1682@gmail.com>
2024-08-10 22:40:27 +0300

    [fix] CORS in api

--- api/src/main.go
+++ api/src/main.go
@@ -7,10 +7,24 @@ import (
 	"net/http"
 )
 
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == http.MethodOptions {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	utils.InitMongo()
 
 	r := routes.SetupRoutes()
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	corsHandler := CORSMiddleware(r)
+
+	log.Fatal(http.ListenAndServe(":8080", corsHandler))
 }
