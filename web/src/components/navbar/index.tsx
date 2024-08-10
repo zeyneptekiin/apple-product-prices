9e1fe5d2b9ec44155d7ab6aa1b4a5fc90b0f5956
zeyneptekin <zeyneptekin1682@gmail.com>
2024-08-10 23:23:29 +0300

    [add] z-index to navbar

--- web/src/components/navbar/index.tsx
+++ web/src/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <nav className="w-full h-16 fixed top-0 bg-orange-50 flex justify-center">
+        <nav className="w-full h-16 fixed top-0 bg-orange-50 flex justify-center z-10">
         <div className="w-[1440px] flex items-center px-8 justify-between text-xl font-medium text-blue-600">
             <p>Global Price Tracking</p>
             <ul className="flex gap-3">
