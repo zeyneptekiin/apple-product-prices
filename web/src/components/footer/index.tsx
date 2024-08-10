e69f341599aaafdb5d1bb771bc868079870b3f26
zeyneptekin <zeyneptekin1682@gmail.com>
2024-08-10 23:22:52 +0300

    [remove] fixed position from footer

--- web/src/components/footer/index.tsx
+++ web/src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full h-8 fixed bottom-0 bg-orange-50 flex justify-center items-center">
+        <footer className="w-full h-8 bottom-0 bg-orange-50 flex justify-center items-center">
             <p className="italic text-orange-500 opacity-70">by zeyneptekin</p>
         </footer>
     )
