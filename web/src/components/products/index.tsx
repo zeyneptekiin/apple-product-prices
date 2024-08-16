4f4727ec72437a4a5b79f22911163a7e3ce5b980
zeyneptekin <zeyneptekin1682@gmail.com>
2024-08-16 03:23:11 +0300

    [adjust] object-contain for product image

--- web/src/components/products/index.tsx
+++ web/src/components/products/index.tsx
@@ -66,7 +66,7 @@ export default function Products({ data = [], lang }: ProductsProps) {
                         <div key={product.product_name} className="grid grid-cols-7 gap-4 mt-16 justify-center items-center">
                             <div className="justify-center items-center">
                                 <div className="w-20 h-20 mx-auto">
-                                    <img src={product.images} alt={product.product_name} className="h-[100%] object-cover mx-auto"/>
+                                    <img src={product.images} alt={product.product_name} className="h-[100%] object-contain mx-auto"/>
                                 </div>
                                 <p className="text-center mt-2">{product.name}</p>
                             </div>
